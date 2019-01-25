7a514f71c0323210eb6428f6a4067f4921da2e1b
cjy513203427 <634208959@qq.com>
2019-01-25 17:19:02 +0800

    remove useless Controller

--- src/main/java/org/format/demo/controller/TestWildcardController.java
+++ src/main/java/org/format/demo/controller/TestWildcardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/wildcard")
+@RequestMapping("wildcard")
 public class TestWildcardController {
 
     @RequestMapping("/test/**")
@@ -22,14 +22,6 @@ public class TestWildcardController {
     public String test2(ModelAndView view) {
         view.setViewName("/test/test");
         view.addObject("attr", "TestWildcardController -> /test*");
-        return String.valueOf(view);
-    }
-
-    @RequestMapping("test?")
-    @ResponseBody
-    public String test3(ModelAndView view) {
-        view.setViewName("/test/test");
-        view.addObject("attr", "TestWildcardController -> test?");
         return String.valueOf(view);
     }
 
